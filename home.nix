eeb9e339a7cf5f1349de9b4ae82b8bfd57291e6e
kalium <kalium222@outlook.com>
2026-07-07 17:52:56 -0400

    feat: with some tools

--- home.nix
+++ home.nix
@@ -10,21 +10,45 @@
     homeDirectory = "/home/Kalium";
     stateVersion = "26.11";
     preferXdgDirectories = true;
+
     packages = with pkgs; [
+      man
+      less
+      curl
+      fastfetch
       neovim
+      zellij
+      bat
+      ripgrep
+      fzf
+      fd
     ];
-    shell.enableBashIntegration = true;
     shellAliases = {
       "ls" = "ls --color=auto";
       "grep" = "grep --color=auto";
       "diff" = "diff --color=auto";
 
-      "v" = "nvim";
       "h" = "home-manager";
+      "v" = "nvim";
+      "f" = "fastfetch";
+      "y" = "yazi_wrapper";
+      "z" = "zellij_wrapper";
     };
     sessionVariables = {
       EDITOR = lib.getExe pkgs.neovim;
+      PAGER = "${lib.getExe pkgs.bat} -p";
+
+      MANROFFOPT = "-c";
+      LESS = "-R";
+      LESS_TERMCAP_mb = ''$'\E[1;31m' '';
+      LESS_TERMCAP_md = ''$'\E[1;36m' '';
+      LESS_TERMCAP_me = ''$'\E[0m' '';
+      LESS_TERMCAP_so = ''$'\E[01;33m' '';
+      LESS_TERMCAP_se = ''$'\E[0m' '';
+      LESS_TERMCAP_us = ''$'\E[1;32m' '';
+      LESS_TERMCAP_ue = ''$'\E[0m' '';
     };
+    shell.enableBashIntegration = true;
   };
 
   nix.assumeXdg = true;
