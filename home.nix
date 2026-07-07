eaa4822e73b98a24ef86190d2b91fb9638b7e148
kalium <kalium222@outlook.com>
2026-07-07 14:10:56 -0400

    feat: basic bash config

--- home.nix
+++ home.nix
@@ -1,23 +1,45 @@
-{ config, pkgs, ... }:
 {
-    xdg.enable = true;
-    nix.assumeXdg = true;
-    targets.genericLinux.enable = true;
-
-    home.username = "Kalium";
-    home.homeDirectory = "/home/Kalium";
-    home.stateVersion = "26.11";
-    home.packages = with pkgs; [
-        neovim
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  home = {
+    username = "Kalium";
+    homeDirectory = "/home/Kalium";
+    stateVersion = "26.11";
+    preferXdgDirectories = true;
+    packages = with pkgs; [
+      neovim
     ];
-    home.sessionVariables = {
-        EDITOR = pkgs.neovim;
+    shell.enableBashIntegration = true;
+    shellAliases = {
+      "ls" = "ls --color=auto";
+      "grep" = "grep --color=auto";
+      "diff" = "diff --color=auto";
+
+      "v" = "nvim";
+      "h" = "home-manager";
     };
-    programs.home-manager.enable = true;
-    programs.bash = {
-        enable = true;
-        historySize = 1000;
-        historyFile = "${config.xdg.stateHome}/histfile";
-        historyFileSize = 1000;
+    sessionVariables = {
+      EDITOR = lib.getExe pkgs.neovim;
     };
+  };
+
+  nix.assumeXdg = true;
+  targets.genericLinux.enable = true;
+  xdg.enable = true;
+
+  programs.home-manager.enable = true;
+  programs.bash = {
+    enable = true;
+    historySize = 1000;
+    historyFile = "${config.xdg.stateHome}/histfile";
+    historyFileSize = 1000;
+
+    initExtra = ''
+      PS1="''${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+    '';
+  };
 }
